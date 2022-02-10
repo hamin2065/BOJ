af752f7f588fed9d534e81e402f0b166fb047f9e
구하민 <hamin2065@gmail.com>
2022-02-10 22:36:43 +0900

    그래프 알고리즘

---
+++ 그래프/사이클판별.py
@@ -0,0 +1,33 @@
+def find_parent(parent, x) : 
+    if parent[x] != x : 
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b) : 
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b : 
+        parent[b] = a
+    else : 
+        parent[a] = b
+        
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int, input().split())
+parent = [i for i in range(v+1)]
+
+cycle = False # 사이클 발생 여부
+
+for i in range(e) : 
+    a, b = map(int, input().split())
+    # 사이클이 발생한 경우 종료
+    if find_parent(parent, a) == find_parent(parent, b) : 
+        cycle = True
+        break
+    # 사이클이 발생하지 않았다면 합집합(union) 수행
+    else : 
+        union_parent(parent, a, b)
+        
+if cycle : 
+    print("사이클이 발생했습니다.")
+else : 
+    print("사이클이 발생하지 않았습니다.")
