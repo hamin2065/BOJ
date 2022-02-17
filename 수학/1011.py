b942703d33c8e4599f2133bd7b03efc1f5ace3cc
구하민 <hamin2065@gmail.com>
2022-02-17 21:13:45 +0900

    수학-1011

---
+++ 수학/1011.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/1011
+# Fly me to the Alpha Centauri
+
+def solve(x, y) : 
+    dis = y-x
+    k = 1
+    count = 0
+    while dis > 0 : 
+        if k*2 <= dis : 
+            dis -= k*2
+            k += 1
+            count += 2
+        elif k <= dis : 
+            dis -= k
+            count += 1
+        else : 
+            dis = 0
+            count += 1
+    return count
+        
+
+n = int(input())
+
+for _ in range(n) : 
+    x, y = map(int, input().split())
+    print(solve(x, y))
+    
