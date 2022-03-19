ff2549594ba97829459f34764923f3ea7d456c50
구하민 <hamin2065@gmail.com>
2022-03-19 23:44:29 +0900

    최단경로-11404

---
+++ 최단경로/11404.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/11404
+# 플로이드
+
+import sys
+INF = int(1e9)
+n = int(input())
+m = int(input())
+
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(1, n+1) : 
+    for j in range(1, n+1) : 
+        if i == j : graph[i][j] = 0
+for _ in range(m) : 
+    a,b,c = map(int, sys.stdin.readline().split())
+    if graph[a][b] > c : 
+        graph[a][b] = c
+for k in range(1, n+1) : 
+    for i in range(1, n+1) : 
+        for j in range(1, n+1) : 
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+            
+for i in range(1, n+1) : 
+    for j in range(1, n+1) : 
+        print(0 if graph[i][j] == INF else graph[i][j], end=' ')
+    print()
